ab164eedeac7d8b5da2ffc2247dc8efb944c16c9
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-12-13 01:23:02 +0530

    fixed bugs

--- WorkoutTracker/src/main/java/com/GymProject/WorkoutTracker/controller/WorkoutSetController.java
+++ WorkoutTracker/src/main/java/com/GymProject/WorkoutTracker/controller/WorkoutSetController.java
@@ -6,7 +6,9 @@ import com.GymProject.WorkoutTracker.service.CreateWorkoutSetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class WorkoutSetController {
 
     @Autowired
